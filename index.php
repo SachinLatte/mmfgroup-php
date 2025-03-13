ec7d45ecd2a1e886d30fc05c5f53175d582a497f
borgesachin365 <borgesachin365@gmail.com>
2025-03-13 22:32:52 +0530

    half of the banner part done

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php include "head.php" ?>
+<?php include "navigation.php" ?>
 <section class="hero-section">
   <div class="hero-carousel-navigations">
     <div class="slider slider-thumb">
       <div class="slider-thumb-content">
         <i class="icons"><img src="img/icon1.svg" alt=""></i>
-        <h4>Smart Solar Technologgy</h4>
+        <h4>Smart Solar <br> Technologgy</h4>
       </div>
       <div class="slider-thumb-content">
         <i class="icons"><img src="img/icon2.svg" alt=""></i>
-        <h4>Advanced Solar Solutions</h4>
+        <h4>Advanced Solar <br> Solutions</h4>
       </div>
       <div class="slider-thumb-content">
         <i class="icons"><img src="img/icon3.svg" alt=""></i>
-        <h4>Unlimited Solar Power</h4>
+        <h4>Unlimited Solar <br> Power</h4>
       </div>
     </div>
   </div>
